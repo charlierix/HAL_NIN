ecf7af289aa1f4e609fb7fedf027364ba7e2559f
charlierix <charlie.rix@gmail.com>
2024-06-22 13:45:26 -0500

    finished implementing mic listener

--- Mic to Text/workers/mic_listener.py
+++ Mic to Text/workers/mic_listener.py
@@ -1,12 +1,50 @@
-# import multiprocessing
-import time
+# https://www.youtube.com/watch?v=k6nIxWGdrS
 
+import datetime
+import pyaudio
+
+FORMAT = pyaudio.paInt16
+RATE = 44100
+CHUNK_LENGTH = 3        # seconds
+
+# Keeps running until the cancel queue is populated
+# Records small sound snippets from the mic and puts them on the queues
+# A sample placed on a queue is CHUNK_LENGTH * 2
+# queue_even gets items starting at time=0, time=0+CHUNK_LENGTH*2 ...
+# queue_odd gets items starting at time=CHUNK_LENGTH, time=CHUNK_LENGTH+CHUNK_LENGTH*2 ...
 def mic_to_soundclips(queue_even, queue_odd, queue_cancel):
-    print("mic_to_soundclips")
-    
+    # how does it know that input is mic and not line in?  maybe a default?
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=FORMAT, channels=1, rate=RATE, input=True, frames_per_buffer=1024)
+
+    chunk = None
+    prev_chunk = None
+    counter = 0
+
     while True:
         # See if the process should stop
         if not queue_cancel.empty():
             break
 
-        time.sleep(1)
+        # Get a small snippet of sound from the mic
+        prev_chunk = chunk
+        chunk = record_chunk(stream)
+
+        # Place on one of the two queues
+        queue = queue_even if counter % 2 == 0 else queue_odd
+        queue.put((datetime.utcnow(), prev_chunk + chunk))
+
+        counter += 1
+    
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+# Records CHUNK_LENGTH seconds of audio into a list
+def record_chunk(stream):
+    frames = []
+    for _ in range(0, int(RATE / 1024 * CHUNK_LENGTH)):
+        data = stream.read(1024)
+        frames.append(data)
+
+    return frames
