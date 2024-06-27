6725b591c7a1d013c3b4af5554f4c45f969db788
charlierix <charlie.rix@gmail.com>
2024-06-27 12:54:49 -0500

    starting to add text filter process

--- Mic to Text/transcribe.py
+++ Mic to Text/transcribe.py
@@ -4,6 +4,7 @@ import multiprocessing
 
 from workers.mic_listener import mic_to_soundclips
 from workers.transcriber import soundclips_to_text
+from workers.text_filter import filter_text_streams
 
 if __name__ == "__main__":
     print("Press backspace to quit...")
@@ -22,15 +23,14 @@ if __name__ == "__main__":
     mic_listener = multiprocessing.Process(target=mic_to_soundclips, args=(queue_even, queue_odd, queue_cancel, config))
     transcriber_even = multiprocessing.Process(target=soundclips_to_text, args=(queue_even, queue_text1, queue_cancel, config))
     transcriber_odd = multiprocessing.Process(target=soundclips_to_text, args=(queue_odd, queue_text1, queue_cancel, config))
+    text_filter = multiprocessing.Process(target=filter_text_streams, args=(queue_text1, queue_text2, queue_cancel, config))
 
-
-    # TODO: Text merge/filter process
-
-
+    # TODO: instead of writing to queue_text2, do http puts
 
     mic_listener.start()
     transcriber_even.start()
     transcriber_odd.start()
+    text_filter.start()
 
     # Wait for backspace key
     # NOTE: keyboard relies on root access
@@ -43,5 +43,6 @@ if __name__ == "__main__":
     mic_listener.join()
     transcriber_even.join()
     transcriber_odd.join()
+    text_filter.join()
 
     print("finished")
---
+++ Mic to Text/workers/text_filter.py
@@ -0,0 +1,73 @@
+import datetime
+import time
+import json
+
+def filter_text_streams(queue_text1, queue_text2, queue_cancel, config):
+    config_filter = config["text_filter"]
+
+    buffer = []     # list of (start, stop, probability, word)
+
+    log = open("log.json", "w")
+
+    # TODO: May also want a small history of probabilities.  when in silence, there are some low probability halucinations, maybe one word
+    # with high probability.  So when in that state, there should be a stronger set of high probabilities before going to just blindly trusting
+    #
+    # It might be good enough to be in silence or active state
+
+    while True:
+        # See if the process should stop
+        if not queue_cancel.empty():
+            log.close()
+            break
+
+        # Pop any words from the queue into the buffer list
+        while not queue_text1.empty():
+            buffer.append(queue_text1.get())
+
+        # Figure out times
+        now = datetime.utcnow()
+        sendwindow_start = now - datetime.timedelta(seconds=config_filter["send_delay_max"])
+        sendwindow_stop = now - datetime.timedelta(seconds=config_filter["send_delay_min"])
+
+        words_in_window = get_words_in_window(buffer, sendwindow_start, sendwindow_stop)
+        #send_words(words_in_window, queue_text2)
+
+        log_window(words_in_window)
+
+        time.sleep(0.333)
+
+
+# Scans buffer for words that are in or before the window
+# If the word is completely before the time window (too far in the past), it is simply removed
+# If the word is in the window, it is removed from the buffer and put in the return list
+# TODO: may want to sort the return list
+def get_words_in_window(buffer, sendwindow_start, sendwindow_stop):
+    retVal = []
+
+    index = len(buffer) - 1     # it looks like len() needs to iterate to get the number, also pop needs to shift things left (I could be mistaken).  So work right to left
+
+    while index >= 0:
+        if buffer[index][1] < sendwindow_start:
+            # Too old, just remove and ignore it
+            print("!! removing word !! '" + buffer[index][3] + "'")
+            buffer.pop(index)
+
+        elif buffer[index][0] > sendwindow_stop:
+            # Too new, change index to look at a different item
+            index -= 1
+
+        else:
+            # In the window, move to return list
+            retVal.append(buffer.pop(index))
+
+    return retVal    
+
+
+def send_words(words_in_window, queue):
+    #pairs = get_pairs(words_in_window)
+    print("finish this (send_words)")
+
+
+def log_window(words_in_window):
+    #json.dump({'grouped_items': grouped_results}, log_file, indent=4)
+    print("finish this (log_window)")
