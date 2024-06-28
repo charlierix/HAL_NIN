632ada5b058f7bddef16d863182295e359568532
charlierix <charlie.rix@gmail.com>
2024-06-27 22:44:50 -0500

    implemented logging
    leaving commas at the end of every block.  Parsers complain, but it's too much code to bother fixing

--- Mic to Text/workers/text_filter.py
+++ Mic to Text/workers/text_filter.py
@@ -3,11 +3,12 @@ import time
 #import json
 
 def filter_text_streams(queue_text1, queue_text2, queue_cancel, config):
-    config_filter = config["text_filter"]
+    config_filter = config['text_filter']
 
     buffer = []     # list of (start, stop, probability, word)
 
-    log = open("log.json", "w")
+    log = open('log.json', 'w')
+    log.write('{"actions": [')
 
     # TODO: May also want a small history of probabilities.  when in silence, there are some low probability halucinations, maybe one word
     # with high probability.  So when in that state, there should be a stronger set of high probabilities before going to just blindly trusting
@@ -17,6 +18,7 @@ def filter_text_streams(queue_text1, queue_text2, queue_cancel, config):
     while True:
         # See if the process should stop
         if not queue_cancel.empty():
+            log.write(']}')
             log.close()
             break
 
@@ -26,32 +28,31 @@ def filter_text_streams(queue_text1, queue_text2, queue_cancel, config):
 
         # Figure out times
         now = datetime.now(timezone.utc)
-        sendwindow_start = now - timedelta(seconds=config_filter["send_delay_max"])
-        sendwindow_stop = now - timedelta(seconds=config_filter["send_delay_min"])
+        sendwindow_start = now - timedelta(seconds=config_filter['send_delay_max'])
+        sendwindow_stop = now - timedelta(seconds=config_filter['send_delay_min'])
 
-        words_in_window = get_words_in_window(buffer, sendwindow_start, sendwindow_stop)
+        words_in_window = get_words_in_window(buffer, sendwindow_start, sendwindow_stop, now, log)
         #send_words(words_in_window, queue_text2)
 
-        log_window(words_in_window)
+        log_window(log, words_in_window, now)
 
         time.sleep(0.333)
-
 
 # Scans buffer for words that are in or before the window
 # If the word is completely before the time window (too far in the past), it is simply removed
 # If the word is in the window, it is removed from the buffer and put in the return list
 # TODO: may want to sort the return list
-def get_words_in_window(buffer, sendwindow_start, sendwindow_stop):
+def get_words_in_window(buffer, sendwindow_start, sendwindow_stop, now, log):
     retVal = []
 
     index = len(buffer) - 1     # it looks like len() needs to iterate to get the number, also pop needs to shift things left (I could be mistaken).  So work right to left
 
     while index >= 0:
-        print("examining word: " + str(buffer[index]))
+        print('examining word: ' + str(buffer[index]))
 
         if buffer[index][1] < sendwindow_start:
             # Too old, just remove and ignore it
-            print("!! removing word !! '" + buffer[index][3] + "'")
+            log_removal(log, buffer[index], sendwindow_start, sendwindow_stop, now)
             buffer.pop(index)
 
         elif buffer[index][0] <= sendwindow_stop:
@@ -60,14 +61,38 @@ def get_words_in_window(buffer, sendwindow_start, sendwindow_stop):
             
         index -= 1
 
-    return retVal    
-
+    return retVal
 
 def send_words(words_in_window, queue):
     #pairs = get_pairs(words_in_window)
-    print("finish this (send_words)")
+    print('finish this (send_words)')
 
+def log_removal(log, word, sendwindow_start, sendwindow_stop, now):
+    log.write('{"remove": {')
 
-def log_window(words_in_window):
-    #json.dump({'grouped_items': grouped_results}, log_file, indent=4)
-    print("finish this (log_window)")
+    log.write('"now": "' + str(now) + '",')
+    log.write('"sendwindow_start": "' + str(sendwindow_start) + '",')
+    log.write('"sendwindow_stop": "' + str(sendwindow_stop) + '",')
+
+    log.write('"start": "' + str(word[0]) + '",')
+    log.write('"stop": "' + str(word[1]) + '",')
+    log.write('"probability": "' + str(word[2]) + '",')
+    log.write('"word": "' + word[3] + '"')
+    log.write('},')
+
+def log_window(log, words_in_window, now):
+    if len(words_in_window) == 0:
+        return False
+    
+    log.write('{"window": [')
+
+    for word in words_in_window:
+        log.write('{')
+        log.write('"now": "' + str(now) + '",')
+        log.write('"start": "' + str(word[0]) + '",')
+        log.write('"stop": "' + str(word[1]) + '",')
+        log.write('"probability": "' + str(word[2]) + '",')
+        log.write('"word": "' + word[3] + '"')
+        log.write('},')
+    
+    log.write(']},')
