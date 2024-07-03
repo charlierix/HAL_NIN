c69adb9548a890c5817d12dc41b9e69e514cb2db
charlierix <charlie.rix@gmail.com>
2024-07-03 00:21:42 -0500

    starting to add logging

--- Mic to Text/transcribe.py
+++ Mic to Text/transcribe.py
@@ -1,9 +1,16 @@
+import datetime
 import json5        # json5 supports json with comments
 import keyboard
 import multiprocessing
+import os
 
 from workers.mic_listener import mic_to_soundclips
 from workers.transcriber import soundclips_to_text
+
+def get_log_folder():
+    retVal = 'logs/' + datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+    os.makedirs(retVal)
+    return retVal
 
 if __name__ == "__main__":
     print('Press backspace to quit...')
@@ -11,14 +18,16 @@ if __name__ == "__main__":
     with open('config.json', 'r') as f:
         config = json5.load(f)
 
+    log_folder = None if not config['should_log'] else get_log_folder()
+
     # These queues are how the processes send messages between themselves
     queue_sound = multiprocessing.Queue()       # receives audio clips (models.SoundClip)
     #queue_text = multiprocessing.Queue()       # receives words (should be http post)
     queue_cancel = multiprocessing.Queue()
 
     # Kick off processes
-    mic_listener = multiprocessing.Process(target=mic_to_soundclips, args=(queue_sound, queue_cancel, config))
-    transcriber = multiprocessing.Process(target=soundclips_to_text, args=(queue_sound, queue_cancel, config))
+    mic_listener = multiprocessing.Process(target=mic_to_soundclips, args=(queue_sound, queue_cancel, config, log_folder))
+    transcriber = multiprocessing.Process(target=soundclips_to_text, args=(queue_sound, queue_cancel, config, log_folder))
     
     # TODO: instead of writing to queue_text, do http posts
 
--- Mic to Text/workers/mic_listener.py
+++ Mic to Text/workers/mic_listener.py
@@ -3,36 +3,44 @@
 from datetime import datetime, timezone
 import numpy as np
 import pyaudio
+import wave
+
 from .models import SoundClip
 
 # Keeps running until the cancel queue is populated
 # Records small sound snippets from the mic and puts them on the queues
 # Items placed on the queues are instances of models.SoundClip
-def mic_to_soundclips(queue_sound, queue_cancel, config):
+def mic_to_soundclips(queue_sound, queue_cancel, config, log_folder):
     config_audio = config['audio']
     rate = config_audio['rate']
     clip_length = config_audio['clip_length']
+    should_log = config['should_log']
 
     # how does it know that input is mic and not line in?  maybe a default?
     audio = pyaudio.PyAudio()
-    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=rate, input=True, frames_per_buffer=1024)
+    format = pyaudio.paInt16
+    stream = audio.open(format=format, channels=1, rate=rate, input=True, frames_per_buffer=1024)
 
     clip = None
+    full_sound = b''
 
     while True:
         # See if the process should stop
         if not queue_cancel.empty():
             break
 
-        clip = record_clip(stream, rate, clip_length)
+        clip = record_clip(stream, rate, clip_length, should_log, full_sound)
         queue_sound.put(clip)     # models.SoundClip
 
     stream.stop_stream()
     stream.close()
     audio.terminate()
 
+    if should_log:
+        write_wav_file(log_folder, audio, format, rate, full_sound)
+
 # Records clip_length seconds of audio into a list
-def record_clip(stream, rate, clip_length):
+def record_clip(stream, rate, clip_length, should_log, full_sound):
     all_data = b''
     start = datetime.now(timezone.utc)
 
@@ -42,4 +50,17 @@ def record_clip(stream, rate, clip_length):
 
     stop = datetime.now(timezone.utc)
 
+    if should_log:
+        full_sound += all_data
+
     return SoundClip(start, stop, np.frombuffer(all_data, np.int16).astype(np.float32) / 32768.0)       # I'm guessing it's 32768 because format is pyaudio.paInt16.  I saw another example that was using 8 bit and they divided by 255
+
+def write_wav_file(log_folder, audio, format, rate, full_sound):
+    filename = log_folder + '/audio.wav'
+
+    sound_file = wave.open(filename, 'wb')     # wb is write bytes mode
+    sound_file.setnchannels(1)
+    sound_file.setsampwidth(audio.get_sample_size(format))
+    sound_file.setframerate(rate)
+    sound_file.writeframes(full_sound)
+    sound_file.close()
