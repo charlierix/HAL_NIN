6e45140b600bf1f21569fd9d0327858a53afd91c
charlierix <charlie.rix@gmail.com>
2024-07-04 23:32:14 -0500

    starting to add a word visualizer controller

---
+++ AudioChat/Controllers/VisualizeWordsController.cs
@@ -0,0 +1,22 @@
+﻿using AudioChat.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AudioChat.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VisualizeWordsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] Word[] words)
+        {
+
+            // TODO: create a singleton wpf window with transparent background
+            // Use a config for size, position, colors, etc
+            // As words come in, pass them to the window, which will show them for a bit and auto clear
+
+
+            return Ok("success");
+        }
+    }
+}
---
+++ AudioChat/Models/Word.cs
@@ -0,0 +1,10 @@
+﻿namespace AudioChat.Models
+{
+    public class Word
+    {
+        public DateTime Start { get; set; }
+        public DateTime Stop { get; set; }
+        public float Probability { get; set; }
+        public string Text { get; set; }
+    }
+}
