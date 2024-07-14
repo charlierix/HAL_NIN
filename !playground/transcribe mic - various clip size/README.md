ba4f83fd8aac2d3602a8c73301665c28c4031854
charlierix <charlie.rix@gmail.com>
2024-07-14 13:15:39 -0500

    adding transcribe mic - various clip size

---
+++ !playground/transcribe mic - various clip size/README.md
@@ -0,0 +1,7 @@
+Keeps streaming from the microphone into half second clips until a key is pressed
+
+It then transcribes those in powers of two: half second clips, one second clips, two seconds ... full recording as a single clip
+
+False/True is a param to the transcriber telling it to remember previous calls
+
+The output is then saved as wav and json files for each transcription
---
+++ !playground/transcribe mic - various clip size/record - transcribe.py
@@ -0,0 +1,172 @@
+from datetime import datetime, timezone, timedelta
+from faster_whisper import WhisperModel
+import json
+import keyboard
+import numpy as np
+import os
+import pyaudio
+import wave
+
+CLIP_LENGTH = 0.5
+FORMAT = pyaudio.paInt16
+RATE = 16000
+
+key_pressed = False     # set by an event listener
+
+class TranscribedWord:
+    def __init__(self, start, stop, probability, word):
+        self.start = start
+        self.stop = stop
+        self.probability = probability
+        self.word = word
+
+# ---------------------------------------------------------------------------------------
+
+def get_base_folder():
+    retVal = 'output/' + datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+    os.makedirs(retVal)
+    return retVal
+
+def on_key_press(event):        # event param is needed to conform to keyboard.on_press interface
+    global key_pressed          # referencing the global so that it doesn't set a local variable with the same name
+    key_pressed = True
+
+# ---------------------------------------------------------------------------------------
+
+def record_sound_clips(audio):
+    stream = audio.open(format=FORMAT, channels=1, rate=RATE, input=True, frames_per_buffer=1024)
+
+    clips = []
+
+    start = datetime.now(timezone.utc)
+    last_print = start
+
+    while True:
+        if key_pressed:
+            break
+
+        now = datetime.now(timezone.utc)
+        if (now - last_print).total_seconds() > 5:
+            last_print = now
+            print('%.1f seconds' % ((now - start).total_seconds()))
+
+        clips.append(record_clip(stream))
+
+    stream.stop_stream()
+    stream.close()
+
+    return clips
+
+# Records clip_length seconds of audio into a list
+def record_clip(stream):
+    retVal = b''
+
+    for _ in range(0, int(RATE / 1024 * CLIP_LENGTH)):
+        retVal += stream.read(1024)
+
+    return retVal
+
+# ---------------------------------------------------------------------------------------
+
+def transcribe(clips, base_folder, audio):
+    power = 0
+
+    while True:
+        span = 2 ** power
+
+        print('transcribing ' + str(span * CLIP_LENGTH) + ' second clips')
+
+        transcribe_span(clips, span, base_folder, audio, False)
+        transcribe_span(clips, span, base_folder, audio, True)
+
+        if span >= len(clips):
+            break
+
+        power += 1
+
+def transcribe_span(clips, span, base_folder, audio, condition_on_previous_text):
+    folder = base_folder + '/' + str(span) + ' ' + str(condition_on_previous_text)
+    os.makedirs(folder)
+
+    model = WhisperModel('distil-large-v3', device='cuda', compute_type='int8_float16')
+
+    index = 0
+
+    while index <= len(clips):
+        clip = extract_clip(clips, index, span)
+
+        words = transcribe_clip(clip[0], model, condition_on_previous_text)
+
+        write_outputs(folder, words, clip[1], audio, index)
+
+        index += span
+
+def extract_clip(clips, index, span):
+    data = b''
+    data_list = []
+
+    for i in range(index, min(index + span, len(clips) - 1)):
+        data += clips[i]
+        data_list.append(clips[i])
+
+    return (np.frombuffer(data, np.int16).astype(np.float32) / 32768.0, data_list)
+
+def transcribe_clip(clip, model, condition_on_previous_text):
+    segments, _ = model.transcribe(clip, language='en', word_timestamps=True, condition_on_previous_text=condition_on_previous_text)
+    retVal = []
+
+    for segment in segments:
+        # NOTE: this needs param: word_timestamps=True
+        for word in segment.words:
+            retVal.append(TranscribedWord(word.start, word.end, word.probability, word.word))
+
+    return retVal
+
+def write_outputs(folder, words, clip, audio, index):
+    filename_base = folder + '/' + str(index)
+
+    write_wav_file(filename_base + '.wav', audio, clip)
+    write_json_file(filename_base + '.json', words)
+
+def write_wav_file(filename, audio, full_sound):
+    sound_file = wave.open(filename, 'wb')     # wb is write bytes mode
+    sound_file.setnchannels(1)
+    sound_file.setsampwidth(audio.get_sample_size(FORMAT))
+    sound_file.setframerate(RATE)
+    sound_file.writeframes(b''.join(full_sound))
+    sound_file.close()
+
+def write_json_file(filename, words):
+    # TranscribedWord isn't serializable.  Need to turn each one into a dictionary
+    words_dicts = []
+    for word in words:
+        words_dicts.append({
+            'start': str(word.start),
+            'stop': str(word.stop),
+            'probability': word.probability,
+            'word': word.word })
+
+    dict = { "words": words_dicts }
+    json_obj = json.dumps(dict, indent=4)
+
+    with open(filename, 'w') as outfile:
+        outfile.write(json_obj)
+
+# ---------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    base_folder = get_base_folder()
+
+    print('Press any key to stop recording...')
+    keyboard.on_press(on_key_press)
+
+    audio = pyaudio.PyAudio()
+
+    clips = record_sound_clips(audio)
+
+    print('Transcribing - clip len: ' + str(len(clips)))
+    transcribe(clips, base_folder, audio)
+
+    audio.terminate()
+
+    print('Finished')
